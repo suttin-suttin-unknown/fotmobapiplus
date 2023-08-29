a7cdc0f51587b2e6368a471490bb33af4afafbfb
RR <rra1011@protonmail.com>
2023-08-28 20:37:02 -0700

    stats

---
+++ stats.py
@@ -0,0 +1,24 @@
+"""
+Helper script for checking storage etc.
+"""
+
+import os
+import psutil
+
+paths = []
+for root, _, files in os.walk("league"):
+    for file in files:
+        paths.append(os.path.join(root, file))
+
+total_size = 0
+for path in paths:
+    total_size += os.path.getsize(path)
+
+total_size = total_size /(1024 * 1024)
+
+print(f"Total Size: {total_size:.2f} MB")
+
+total_storage = psutil.disk_usage("/").free
+total_storage = total_storage / (1024 ** 3)
+
+print(f"Total storage available: {total_storage:.2f} GB")
