cd6c091a46d1bcfe4c6313c23b1c3b5b4404fc6a
RR <rra1011@protonmail.com>
2023-09-11 15:35:35 -0700

    add log config

--- utils.py
+++ utils.py
@@ -1,4 +1,19 @@
 import re
+import sys
+
+from loguru import logger
+
+logger.configure(
+    handlers=[
+        {
+            "sink": sys.stdout,
+            "format": "[<green>{time:YYYY-MM-DD HH:mm:ss}</green>] | <level>{level: <8}: {message}</level>",
+            "level": "TRACE",
+            "colorize": True,
+            "backtrace": True,
+        }
+    ]
+)
 
 def convert_camel_to_snake(cc_str):
     return re.sub(r"(?<!^)(?=[A-Z])", "_", cc_str).lower()
